5a2d3ceb274592f75229a7a35918766249d3de5a
Lakshya Kumar <lkkumar544@gmail.com>
2022-03-13 23:28:00 +0530

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div>Init</div>;
+import { Component } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import AppContainerPage from "./Pages/AppContainerPage/AppContainer.page";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/home" />
+            </Route>
+            <Route
+              path={["/home", "/edit/:id", "/new", "/details/:id"]}
+              component={AppContainerPage}
+            />
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
 
 export default App;
